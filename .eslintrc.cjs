664fb53dd0574ba2195d31f04ca9a76da0233c73
Rhenald <renaldkarel@gmail.com>
2023-06-06 15:57:50 +0700

    chore: setup eslint with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,19 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-    'react/prop-types': 0,
-  },
-}
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+    'prettier',
+	],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		'react/prop-types': 0,
+		'no-console': 'warn',
+    'no-unused-vars': 'warn',
+	},
+};
