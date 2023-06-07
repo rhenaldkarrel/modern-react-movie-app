aad27cc0615b35d67421440f1390bc63ff64a91b
Rhenald <renaldkarel@gmail.com>
2023-06-07 22:06:00 +0700

    feat: add slice movies helper

--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -5,3 +5,11 @@ export function getYear(date) {
 		return date.substring(0, 4);
 	}
 }
+
+export function sliceMovies(movies, limit) {
+	if (limit === 0) {
+		return movies;
+	}
+
+	return movies.slice(0, limit);
+}
