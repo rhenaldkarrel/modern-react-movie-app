4af19122488a272975ceba06d84cd8d536fefd54
Rhenald <renaldkarel@gmail.com>
2023-06-07 11:53:25 +0700

    style: adjust navigation styling

--- src/components/common/navigation/NavItem.jsx
+++ src/components/common/navigation/NavItem.jsx
@@ -2,7 +2,10 @@ import { Link } from 'react-router-dom';
 
 const NavItem = ({ children, to, label }) => {
 	return (
-		<Link to={to} className="flex items-center gap-2">
+		<Link
+			to={to}
+			className="flex items-center gap-2 transition-all rounded-lg hover:bg-gray-50 hover:text-black p-3 lg:py-7"
+		>
 			{children}
 			<span className="hidden sm:block lg:hidden capitalize">{label}</span>
 		</Link>
