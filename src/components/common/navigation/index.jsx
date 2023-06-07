966bad2e2a93647cb11775bd6d4f822d460cc1e5
Rhenald <renaldkarel@gmail.com>
2023-06-07 11:46:35 +0700

    style: - change navigation height class - add text size class to nav item icon

--- src/components/common/navigation/index.jsx
+++ src/components/common/navigation/index.jsx
@@ -4,15 +4,15 @@ import NavItem from './NavItem';
 
 export function Navigation() {
 	return (
-		<nav className="sticky top-0 z-50 flex items-center justify-between bg-slate-900 p-5 md:mx-6 md:mt-6 md:mb-[33px] md:rounded-[10px] lg:fixed lg:left-0 lg:mr-0 lg:h-5/6 lg:flex-col lg:py-9 text-white">
+		<nav className="sticky top-0 z-50 flex items-center justify-between bg-slate-900 p-5 md:mx-6 md:mt-6 md:mb-[33px] md:rounded-[10px] lg:fixed lg:left-0 lg:mr-0 lg:h-4/6 lg:flex-col lg:py-9 text-white">
 			<NavItem label="home">
-				<BsFillHouseDoorFill />
+				<BsFillHouseDoorFill className="text-3xl" />
 			</NavItem>
 			<NavItem label="movies">
-				<MdMovieCreation />
+				<MdMovieCreation className="text-3xl" />
 			</NavItem>
 			<NavItem label="TVs">
-				<BsFillTvFill />
+				<BsFillTvFill className="text-3xl" />
 			</NavItem>
 		</nav>
 	);
