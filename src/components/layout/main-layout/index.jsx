1832930bb0e1354cbb7ed798491efe9bb4c0ef76
Rhenald <renaldkarel@gmail.com>
2023-06-07 15:05:23 +0700

    refactor: - separate suspense fallback spinner - adjust `main` styling

--- src/components/layout/main-layout/index.jsx
+++ src/components/layout/main-layout/index.jsx
@@ -3,21 +3,23 @@ import { Outlet } from 'react-router-dom';
 
 import { Navigation, Spinner } from '@/components';
 
+function FallbackSpinner() {
+	return (
+		<div className="h-screen w-full flex items-center justify-center">
+			<Spinner />
+		</div>
+	);
+}
+
 export function MainLayout() {
 	return (
 		<React.Fragment>
 			<Navigation />
-			<main className="mx-0 flex flex-col py-6 px-4 md:m-6 md:px-0 md:pt-0 lg:ml-32 lg:min-w-[800px] lg:grow">
-				<React.Suspense
-					fallback={
-						<div className="h-screen w-full flex items-center justify-center">
-							<Spinner />
-						</div>
-					}
-				>
+			<React.Suspense fallback={<FallbackSpinner />}>
+				<main className="lg:min-w-[800px] lg:ml-32 mx-6 mt-6 md:mt-0 lg:mx-0 lg:py-6">
 					<Outlet />
-				</React.Suspense>
-			</main>
+				</main>
+			</React.Suspense>
 		</React.Fragment>
 	);
 }
