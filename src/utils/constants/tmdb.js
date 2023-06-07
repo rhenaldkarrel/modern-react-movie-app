08a31f8bf3058e928b4f230c1b263f43c489a334
Rhenald <renaldkarel@gmail.com>
2023-06-07 20:05:29 +0700

    chore: fix tmdb image base url path

--- src/utils/constants/tmdb.js
+++ src/utils/constants/tmdb.js
@@ -1,4 +1,4 @@
 export const TMDB_API_KEY = import.meta.env.VITE_APP_TMDB_API_KEY;
 export const TMDB_ACCESS_TOKEN = import.meta.env.VITE_APP_TMDB_ACCESS_TOKEN;
 export const TMDB_API_BASE_URL = 'https://api.themoviedb.org/3/';
-export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
