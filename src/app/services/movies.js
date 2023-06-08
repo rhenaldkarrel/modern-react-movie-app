44e6fc5aaf3185d57c848e586d0998b0a10c33d6
Rhenald <renaldkarel@gmail.com>
2023-06-08 10:58:13 +0700

    feat: add now playing movies query

--- src/app/services/movies.js
+++ src/app/services/movies.js
@@ -24,7 +24,30 @@ export const moviesApi = tmdbApi.injectEndpoints({
 			},
 			providesTags: ['PopularMovies'],
 		}),
+		getNowPlayingMovies: build.query({
+			query: (arg) => {
+				const { page } = arg;
+
+				return {
+					url: 'movie/now_playing',
+					params: {
+						page,
+					},
+				};
+			},
+			serializeQueryArgs: ({ endpointName }) => {
+				return endpointName;
+			},
+			merge: (currentCache, newItems) => {
+				currentCache.results.push(...newItems.results);
+			},
+			forceRefetch({ currentArg, previousArg }) {
+				return currentArg !== previousArg;
+			},
+			providesTags: ['NowPlayingMovies'],
+		}),
 	}),
 });
 
-export const { useGetPopularMoviesQuery } = moviesApi;
+export const { useGetPopularMoviesQuery, useGetNowPlayingMoviesQuery } =
+	moviesApi;
