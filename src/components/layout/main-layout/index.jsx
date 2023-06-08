79537c7f0c2705a424c07ba15258e5856022d067
Rhenald <renaldkarel@gmail.com>
2023-06-08 22:25:02 +0700

    feat: add welcome message if session exist

--- src/components/layout/main-layout/index.jsx
+++ src/components/layout/main-layout/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 
 import { Navigation, SearchMovieForm, Spinner } from '@/components';
+import { useAuth } from '@/hooks';
 
 function FallbackSpinner() {
 	return (
@@ -13,6 +14,7 @@ function FallbackSpinner() {
 
 export function MainLayout() {
 	const { pathname } = useLocation();
+	const { user } = useAuth();
 
 	React.useEffect(() => {
 		window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -23,6 +25,15 @@ export function MainLayout() {
 			<Navigation />
 			<React.Suspense fallback={<FallbackSpinner />}>
 				<main className="lg:min-w-[800px] px-6">
+					{user && (
+						<h1 className="mb-6">
+							Welcome back,{' '}
+							<span className="text-indigo-600">
+								{user.user_metadata.full_name ?? user.email}
+							</span>
+							!
+						</h1>
+					)}
 					<div className="mb-8">
 						<SearchMovieForm />
 					</div>
