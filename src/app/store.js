cd95060d6ad29fa4ede038177ca327e045df6bc3
Rhenald <renaldkarel@gmail.com>
2023-06-13 12:06:18 +0700

    feat: integrate redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { tmdbApi } from './services/api';
 import { moviesReducer } from '@/features';
 
+const persistedMoviesReducer = persistReducer(
+	{
+		key: 'root',
+		storage,
+	},
+	moviesReducer
+);
+
 export const store = configureStore({
 	reducer: {
 		[tmdbApi.reducerPath]: tmdbApi.reducer,
-		movies: moviesReducer,
+		movies: persistedMoviesReducer,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware({ serializableCheck: false }).concat(
 			tmdbApi.middleware
 		),
 });
+
+export const persistor = persistStore(store);
