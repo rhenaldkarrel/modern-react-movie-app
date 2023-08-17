dd05cfb5aadbd9122b5518445e52efdeb76ce95a
Rhenald <renaldkarel@gmail.com>
2023-08-17 17:15:57 +0700

    feat: add skeleton loading on now playing page

--- src/pages/movie/now-playing/index.jsx
+++ src/pages/movie/now-playing/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import { useGetNowPlayingMoviesQuery } from '@/app/services/movies';
-import { MovieList, Spinner } from '@/components';
+import { MovieList, MovieSkeleton, Spinner, TextSkeleton } from '@/components';
 import { useInfiniteScroll } from '@/hooks';
 
 export function MovieNowPlaying() {
 	const [page, setPage] = React.useState(1);
-	const { data, isFetching } = useGetNowPlayingMoviesQuery({ page });
+	const { data, isFetching, isLoading } = useGetNowPlayingMoviesQuery({ page });
 
 	const nowPlayingMovies = data?.results ?? [];
 	const totalPages = data?.total_pages;
@@ -17,6 +17,15 @@ export function MovieNowPlaying() {
 		page,
 		setPage,
 	});
+
+	if (isLoading) {
+		return (
+			<div className="space-y-4">
+				<TextSkeleton height={36} width={234} />
+				<MovieSkeleton amount={10} />
+			</div>
+		);
+	}
 
 	return (
 		<React.Fragment>
